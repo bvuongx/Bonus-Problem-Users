60f0b243ea63b814a5e0507162e93924eab36d75
bvuongx <bvuong@gmail.com>
2013-06-22 20:41:27 -0700

    Bonus Problem Users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Users::Application.config.secret_token = 'c1b6297db6f5d9a77e1f104a8563a922b977e9a284277e082ef777665fa3b0f381f4bedbdc8cbd5d9c9856aab649a61ad4ca7365c2d8c8d327719189212e3eec'
